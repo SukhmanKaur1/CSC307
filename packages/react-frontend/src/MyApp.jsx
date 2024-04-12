4542efc8b8cd1d942707309362b800b5585d8aa3
SukhmanKaur1 <skaur31@calpoly.edu>
2024-04-12 16:46:03 -0700

    STep 6

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,30 +1,37 @@
-
-import React from "react";
+import React, { useState } from "react";
 import Table from "./Table";
 
-const characters = [
-  {
-    name: "Charlie",
-    job: "Janitor"
-  },
-  {
-    name: "Mac",
-    job: "Bouncer"
-  },
-  {
-    name: "Dee",
-    job: "Aspiring actress"
-  },
-  {
-    name: "Dennis",
-    job: "Bartender"
-  }
-];
-
 function MyApp() {
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      name: "Dee",
+      job: "Aspiring actress"
+    },
+    {
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]);
+
+  function removeOneCharacter(index) {
+    const updatedCharacters = characters.filter((_, i) => i !== index);
+    setCharacters(updatedCharacters);
+  }
+
   return (
     <div className="container">
-      <Table characterData={characters} />
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+      />
     </div>
   );
 }
