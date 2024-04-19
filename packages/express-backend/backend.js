eb2eba3445f58010d0bab8e00d9cdad884c0544d
SukhmanKaur1 <skaur31@calpoly.edu>
2024-04-19 16:06:28 -0700

    \users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,34 @@
-// backend.js
+// Step 1: Define the Users Data Structure
+const users = {
+    users_list: [
+        {
+            id: "xyz789",
+            name: "Charlie",
+            job: "Janitor"
+        },
+        {
+            id: "abc123",
+            name: "Mac",
+            job: "Bouncer"
+        },
+        {
+            id: "ppp222",
+            name: "Mac",
+            job: "Professor"
+        },
+        {
+            id: "yat999",
+            name: "Dee",
+            job: "Aspiring actress"
+        },
+        {
+            id: "zap555",
+            name: "Dennis",
+            job: "Bartender"
+        }
+    ]
+};
+
 import express from "express";
 
 const app = express();
@@ -8,6 +38,11 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+
+app.get("/users", (req, res) => {
+    res.send(users);
 });
 
 app.listen(port, () => {
